cc898d3f39dbeda6864c04f234f2e84841617766
Arun <kingarun2020@gmail.com>
2023-09-26 22:01:34 +0545

    props added for the cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Card1 from './Card1'
+import img2 from "../src/paris.jpg"
+import img3 from "../src/san.jpg"
 
 
 const App = () => {
+  let link1 = 'https://media.istockphoto.com/id/615398376/photo/new-york-city-nyc-usa.jpg?s=612x612&w=0&k=20&c=rlrsrt4jbORPDSOW5df06Ik_X_5iQo1rYQd53xSs4nw='
   return (
     <>
     <div className='body'>
@@ -19,7 +22,9 @@ const App = () => {
 
     </div>
     <div className='cards'>
-    <Card1/> <Card1/> <Card1/>
+    <Card1 name="New York" date="Fri 27 Nov 2016" detail="Lorem, ipsum dolor sit amet consectetur adipisicing elit." imageSrc={link1} />
+     <Card1 name="Paris" date="Sat 28 Nov 2016" detail="Lorem, ipsum dolor sit amet consectetur adipisicing elit" imageSrc={img2} />
+      <Card1 name="San Fransisco" date="Sun 29 Nov 2016" detail="Lorem, ipsum dolor sit amet consectetur adipisicing elit" imageSrc={img3} />
     </div>
 
     
--- src/Card1.jsx
+++ src/Card1.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Card1 = () => {
+const Card1 = ({name,date,detail,imageSrc}) => {
   return (
     <>
     <div className='card'>
-        <img src="https://media.istockphoto.com/id/615398376/photo/new-york-city-nyc-usa.jpg?s=612x612&w=0&k=20&c=rlrsrt4jbORPDSOW5df06Ik_X_5iQo1rYQd53xSs4nw=" alt="" />
-        <p>New York</p>
-        <p>Fri 27 Nov 2016</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <img src= {imageSrc} alt="newyork" />
+        <p>{name}</p>
+        <p>{date}</p>
+        <p>{detail}</p>
         <button>Buy Tickets</button>
     </div>
     
