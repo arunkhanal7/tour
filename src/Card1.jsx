e15ce8b2df9e15385dd1b3add73b4505c93a041e
Arun <kingarun2020@gmail.com>
2023-09-26 12:24:07 +0545

    first commit

---
+++ src/Card1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card1 = () => {
+  return (
+    <>
+    <div className='card'>
+        <img src="https://media.istockphoto.com/id/615398376/photo/new-york-city-nyc-usa.jpg?s=612x612&w=0&k=20&c=rlrsrt4jbORPDSOW5df06Ik_X_5iQo1rYQd53xSs4nw=" alt="" />
+        <p>New York</p>
+        <p>Fri 27 Nov 2016</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>Buy Tickets</button>
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Card1
---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='september'>September
+        <span className='sold'>sold out</span>
+    
+        </div>
+        <div className='october'>October
+        <span className='sold'>sold out</span>
+    
+        </div>
+        <div className='november'>November</div>
+    
+
+    </>
+  )
+}
+
+export default Navbar
